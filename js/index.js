064fc59de410a7a91dc4f7381763080bca52870c
rasedul99 <rasedul2199@gmail.com>
2022-03-01 18:53:04 +0600

    card

--- js/index.js
+++ js/index.js
@@ -4,5 +4,26 @@ getPhone = () => {
   //   console.log(url);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((result) => setPhone(result.data));
+};
+
+const setPhone = (phones) => {
+  console.log(phones);
+  for (const phone of phones) {
+    console.log(phone);
+    const phonesContainer = document.getElementById("phones-container");
+    const div = document.createElement("div");
+    div.classList.add("col-md-4");
+    div.innerHTML = `
+    <div class="card my-3">
+    <img src="${phone.image}"  class="card-img-top w-50 mx-auto py-2" alt="${phone.phone_name}"/>
+    <div class="card-body">
+      <h5 class="card-title">${phone.phone_name}</h5>
+      
+      <a href="#" class="btn btn-primary">Details</a>
+    </div>
+  </div>
+    `;
+    phonesContainer.appendChild(div);
+  }
 };
